b01b28e284ff9ab60330dd7fae18f5d21081e573
Andrew Louis <andrew@hyfen.net>
2010-12-06 19:10:10 -0500

    Comments

--- parser.rb
+++ parser.rb
@@ -9,6 +9,8 @@ rows = FasterCSV.read("data/customresult.csv")
 # remove headers
 rows.slice!(0) 
 
+# CSV assumes columns [category, hour, count]
+# example input: "Recycling",23,12
 rows.each{|row|
   unless groups[row[0]]
     groups[row[0]] = [].fill(0, 0, 23)  # make sure arrays are 0 filled
@@ -16,6 +18,7 @@ rows.each{|row|
   groups[row[0]][row[1].to_i] = row[2].to_i
 }
 
+# example output: 1: [0,6,0,0,0,0,2,4,8,10,44,16,26,22,32,10,14,0,2,2,0,2,0,2],
 outputFile = File.new("data/output", "w")
 groups.each{|group_name, group|
   count += 1
@@ -25,7 +28,7 @@ groups.each{|group_name, group|
   outputFile.puts(output)
 }
 
-# copy from stdout for the datalabels variable
+# copy this from stdout to get the datalabels variable for the graph
 count = 0
 groups.each{|group_name, x|
   count += 1
